8f59ea57ea4a8d5819b916b4e8f152508f55985d
Pam Adams <pkadams@epotential.com>
2019-11-18 18:04:22 -0500

    get and create routes finished

--- bookmarks-back-end/controllers/bookmarks.js
+++ bookmarks-back-end/controllers/bookmarks.js
@@ -1,8 +1,46 @@
 const express = require('express')
 const bookmarks = express.Router()
+const Bookmark = require('../models/bookmarks.js')
 
+
+///////////////ROUTES//////////////////////////
+
+//index - get
 bookmarks.get('/', (req, res) => {
-    res.send('BOOKMARKS WITH Natalia and Pam!')
+    //res.send('BOOKMARKS WITH Natalia and Pam!')
+    Bookmark.find({}, (error, foundBookmarks) => {
+        if (error) {
+            res.status(400).json({ error: errormessage })
+        }
+        res.status(200).send(foundBookmarks)
+    })
 })
 
-module.exports = bookmarks
+
+//create
+bookmarks.post('/', (req, res) => {
+    Bookmark.create(req.body, (error, createdBookmark) => {
+        if (error) {
+            res.status(400).json({ error: errormessage })
+        }
+        res.status(200).send(createdBookmark)
+
+    })
+})
+
+bookmarks.delete('/:id', (req, res) => {
+    Bookmark.findByIdAndRemove(req.params.id, (error, deletedBookmark) => {
+        if (error) {
+            res.status(400).json({ error: errormessage })
+        }
+        res.status(200).send(deletedBookmark)
+
+    })
+
+
+
+
+
+
+
+    module.exports = bookmarks
--- bookmarks-back-end/server.js
+++ bookmarks-back-end/server.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const bookmarksController = require('./controllers/bookmarks.js')
 const port = 3003
+const mongoose = require('mongoose')
 
 
 
+// middleware
+app.use(express.json());
 app.use('/bookmarks', bookmarksController)
 
+
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+
+
+mongoose.connect('mongodb://localhost:27017/bookmarks', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoose...')
+})
 app.listen(port, () => {
     console.log("London Calling")
 });
